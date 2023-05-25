9a29a6711e6559124c639dbb3d9bf8aafb45cda5
Piotr <piotrek.zaranek@gmail.com>
2023-05-25 12:19:30 +0200

    Added book model.

---
+++ GiveTheBookBack.Model/Model/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiveTheBookBack.Domain.Model
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public int NumberOfPages { get; set; }
+        public string Description { get; set; }
+    }
+}
