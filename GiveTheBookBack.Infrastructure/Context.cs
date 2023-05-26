33db6886beb1b054e75e3266599d93172131afbb
Piotr <piotrek.zaranek@gmail.com>
2023-05-26 09:39:58 +0200

    Add primary context configuration
    Added DbSets for models.
    Added constructor.
    Added OnModelCreating override method.

--- GiveTheBookBack.Infrastructure/Context.cs
+++ GiveTheBookBack.Infrastructure/Context.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GiveTheBookBack.Domain.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,18 @@ using System.Threading.Tasks;
 
 namespace GiveTheBookBack.Infrastructure
 {
-    public class Context
+    public class Context : DbContext
     {
+        DbSet<Book> Books { get; set; }
+        DbSet<User> Users { get; set; }
+        DbSet<Address> Addresses { get; set; }
+        DbSet<Transaction> Transactions { get; set; }
+
+        public Context(DbContextOptions<Context> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);            
+        }
     }
 }
