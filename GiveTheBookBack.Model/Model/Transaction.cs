93eedebf591a1d253b1962a5fc56b4728af561b2
Piotr <piotrek.zaranek@gmail.com>
2023-05-31 08:55:23 +0200

    Removed User type properties in Transaction.

--- GiveTheBookBack.Model/Model/Transaction.cs
+++ GiveTheBookBack.Model/Model/Transaction.cs
@@ -9,10 +9,8 @@ namespace GiveTheBookBack.Domain.Model
     public class Transaction
     {
         public int Id { get; set; }
-        public int GiverRef { get; set; }
-        public User Giver { get; set; }
-        public int RecipientRef { get; set; }
-        public User Recipient { get; set; }
+        public int GiverRef { get; set; }        
+        public int RecipientRef { get; set; }        
         public int BookRef { get; set; }
         public Book Book { get; set; }
         public DateTime Date { get; set; }
