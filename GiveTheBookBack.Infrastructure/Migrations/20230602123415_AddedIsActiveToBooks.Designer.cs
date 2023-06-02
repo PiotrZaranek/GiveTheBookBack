d71b1245602276bfe9659f69ebc6581f55088a94
Piotr <piotrek.zaranek@gmail.com>
2023-06-02 14:35:34 +0200

    Do migration on database
    Added field named IsActive to Books table.

--- GiveTheBookBack.Infrastructure/Migrations/20230602123415_AddedIsActiveToBooks.Designer.cs
+++ GiveTheBookBack.Infrastructure/Migrations/20230602123415_AddedIsActiveToBooks.Designer.cs
@@ -4,6 +4,7 @@ using GiveTheBookBack.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GiveTheBookBack.Infrastructure.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20230602123415_AddedIsActiveToBooks")]
+    partial class AddedIsActiveToBooks
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
