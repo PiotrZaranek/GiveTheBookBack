f93215e7751de921408f2ded09321cd4f19fe70a
Piotr <piotrek.zaranek@gmail.com>
2023-06-03 08:59:38 +0200

    Added IBookService interface for BookService.

---
+++ GiveTheBookBack.Application/Interface/IBookService.cs
@@ -0,0 +1,18 @@
+﻿using GiveTheBookBack.Application.ViewModel;
+using GiveTheBookBack.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiveTheBookBack.Application.Interface
+{
+    public interface IBookService
+    {
+        int Add(NewBookVm bookVm);
+        int Delete(int id);
+        ListBookForListVm GetAll();
+        BookForDetailsVm Details(int id);
+    }
+}
