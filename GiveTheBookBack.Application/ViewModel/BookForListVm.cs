33ff3a27b1e94a0052519118b11293f6d9bd4114
Piotr <piotrek.zaranek@gmail.com>
2023-06-04 09:21:41 +0200

    Add mapping in view model
    Added IMapFrom interface to BookForDetailsVm, BookForListVm and
    NewBookVm. Implemented Mapping method.

--- GiveTheBookBack.Application/ViewModel/BookForListVm.cs
+++ GiveTheBookBack.Application/ViewModel/BookForListVm.cs
@@ -1,4 +1,6 @@
-﻿using GiveTheBookBack.Domain.Model;
+﻿using AutoMapper;
+using GiveTheBookBack.Application.Mapper;
+using GiveTheBookBack.Domain.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,10 +9,15 @@ using System.Threading.Tasks;
 
 namespace GiveTheBookBack.Application.ViewModel
 {
-    public class BookForListVm
+    public class BookForListVm : IMapFrom<Book>
     {
         public int Id { get; set; }
         public string Title { get; set; }
         public User User { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Book, BookForListVm>();
+        }
     }
 }
