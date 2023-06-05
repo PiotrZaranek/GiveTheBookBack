89044e10ae377f44567dc18798b761f7c571075f
Piotr <piotrek.zaranek@gmail.com>
2023-06-05 15:46:39 +0200

    Add static Setup class
    Added CreateBookService static method.

---
+++ GiveTheBookBack.Test/Services/Setup.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using GiveTheBookBack.Application.Service;
+using GiveTheBookBack.Domain.Interface;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiveTheBookBack.Test.Services
+{
+    public static class Setup
+    {
+        public static BookService CreateBookService() 
+        {
+            var repo = new Mock<IBookRepository>();
+            var mapp = new Mock<IMapper>();
+            return new BookService(repo.Object, mapp.Object);
+        }
+    }
+}
