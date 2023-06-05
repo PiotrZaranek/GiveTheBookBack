fc227cacf076fe4efa1d28dead267b0754b79c14
Piotr <piotrek.zaranek@gmail.com>
2023-06-05 14:43:52 +0200

    Add test for BookService.Add
    Added two tests for BookRepository Add method.
    1. Checks if the BookRepository.Add has been invoked.
    2. Checks return type from Add method.

--- GiveTheBookBack.Test/Services/BookServiceTest/AddTest.cs
+++ GiveTheBookBack.Test/Services/BookServiceTest/AddTest.cs
@@ -1,13 +1,44 @@
-﻿using System;
+﻿using AutoMapper;
+using FluentAssertions;
+using GiveTheBookBack.Application.Service;
+using GiveTheBookBack.Application.ViewModel;
+using GiveTheBookBack.Domain.Interface;
+using GiveTheBookBack.Domain.Model;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GiveTheBookBack.Test.Services.BookServicesTest
-{
+{    
     public class AddTest
     {
-        Book book = new Book();
+        [Fact]
+        public void Add_ShouldInvoke_BookRepositoryAdd()
+        {
+            var repo = new Mock<IBookRepository>();
+            var mapp = new Mock<IMapper>();
+            var ser = new BookService(repo.Object, mapp.Object);
+            
+            var result = ser.Add(It.IsAny<NewBookVm>());
+
+            repo.Verify(m => m.Add(It.IsAny<Book>()), Times.Once());
+        }
+
+        [Fact]
+        public void AddReturnType_ShouldBe_Int()
+        {
+            var repo = new Mock<IBookRepository>();
+            var mapp = new Mock<IMapper>();
+            var ser = new BookService(repo.Object, mapp.Object);
+            repo.Setup(m => m.Add(It.IsAny<Book>())).Returns(Task<int>.FromResult(1));
+
+            var result = ser.Add(It.IsAny<NewBookVm>());
+            
+            Assert.IsType<int>(result);
+            result.Should().Be(1);            
+        }
     }
 }
