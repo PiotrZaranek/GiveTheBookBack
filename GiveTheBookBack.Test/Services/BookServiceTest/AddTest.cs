09480bed525b57ca3659b53311a4d57173c842fb
Piotr <piotrek.zaranek@gmail.com>
2023-06-05 13:53:46 +0200

    Renamed folder from Book to BookServiceTest

--- GiveTheBookBack.Test/Services/BookServiceTest/AddTest.cs
+++ GiveTheBookBack.Test/Services/BookServiceTest/AddTest.cs
@@ -4,10 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GiveTheBookBack.Test.Services.Book
+namespace GiveTheBookBack.Test.Services.BookServicesTest
 {
     public class AddTest
     {
-                
+        Book book = new Book();
     }
 }
