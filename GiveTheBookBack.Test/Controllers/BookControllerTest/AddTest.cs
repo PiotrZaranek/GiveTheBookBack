f4a28dbe5a8076de32a9c92465358d1a8b4652b1
Piotr <piotrek.zaranek@gmail.com>
2023-06-07 12:28:18 +0200

    Add tests for controllers
    Added folder named Controllers.
    Added tests for BookController.Add method.

---
+++ GiveTheBookBack.Test/Controllers/BookControllerTest/AddTest.cs
@@ -0,0 +1,29 @@
+﻿using GiveTheBookBack.Application.Interface;
+using GiveTheBookBack.API.Controllers;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GiveTheBookBack.Application.ViewModel;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GiveTheBookBack.Test.Controllers.BookControllerTest
+{
+    public class AddTest
+    {
+        [Fact]
+        public void Add_ShouldReturn_Ok200_IfBookWasSuccesfullAdded()
+        {
+            var ser = new Mock<IBookService>();
+            var con = new BookController(ser.Object);
+            ser.Setup(m => m.Add(It.IsAny<NewBookVm>())).Returns(1);
+
+            var result = con.Add(It.IsAny<NewBookVm>());
+
+            Assert.IsType<OkObjectResult>(result);
+        }
+    }
+}
