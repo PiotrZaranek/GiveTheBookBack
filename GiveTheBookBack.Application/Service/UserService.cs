a362f88e3d06a64516dc572ea5b476595c81fe05
Piotr <piotrek.zaranek@gmail.com>
2023-06-13 13:58:18 +0200

    Added implementation of methods without Authenticate method.

--- GiveTheBookBack.Application/Service/UserService.cs
+++ GiveTheBookBack.Application/Service/UserService.cs
@@ -1,6 +1,8 @@
-﻿using GiveTheBookBack.Application.Interface;
+﻿using AutoMapper;
+using GiveTheBookBack.Application.Interface;
 using GiveTheBookBack.Application.ViewModel;
 using GiveTheBookBack.Domain.Interface;
+using GiveTheBookBack.Domain.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,15 +14,18 @@ namespace GiveTheBookBack.Application.Service
     public class UserService : IUserService
     {
         readonly private IUserRepository _repository;
+        readonly private IMapper _mapper;
 
-        public UserService(IUserRepository repository)
+        public UserService(IUserRepository repository, IMapper mapper)
         {
             _repository = repository;
+            _mapper = mapper;
         }
 
         public void AddAddress(NewAddressVm addressVm)
         {
-            throw new NotImplementedException();
+            var address = _mapper.Map<Address>(addressVm);
+            _repository.AddAddress(address);
         }
 
         public bool Authenticate(LoginUserVm loginModel)
@@ -30,17 +35,28 @@ namespace GiveTheBookBack.Application.Service
 
         public void EditAddress(AddressForEditVm addressVm)
         {
-            throw new NotImplementedException();
+            var address = _mapper.Map<Address>(addressVm);
+            _repository.EditAddress(address);
         }
 
         public AddressForEditVm GetAddressForEdit(int id)
         {
-            throw new NotImplementedException();
+            var address = _repository.GetAddress(id);
+
+            if(address != null)
+            {
+                return _mapper.Map<AddressForEditVm>(address);
+            }            
+            else
+            {
+                return null;
+            }
         }
 
         public void Registration(NewUserVm userVm)
         {
-            throw new NotImplementedException();
+            var user = _mapper.Map<User>(userVm);
+            _repository.Registration(user);
         }
     }
 }
