281f18773074c0f86c17f9bc606ca284aad10aa2
Piotr <piotrek.zaranek@gmail.com>
2023-06-13 11:06:12 +0200

    Added ViewModels AddressForEdit and LoginUser.

---
+++ GiveTheBookBack.Application/ViewModel/AddressForEditVm.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using GiveTheBookBack.Application.Mapper;
+using GiveTheBookBack.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiveTheBookBack.Application.ViewModel
+{
+    public class AddressForEditVm : IMapFrom<Address>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Country { get; set; }
+        public string Region { get; set; }
+        public string City { get; set; }
+        public string PhoneNumber { get; set; }
+        public int UserRef { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Address, AddressForEditVm>().ReverseMap();
+        }
+    }
+}
---
+++ GiveTheBookBack.Application/ViewModel/LoginUserVm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiveTheBookBack.Application.ViewModel
+{
+    public class LoginUserVm
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
