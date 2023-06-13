94377d6618fb752c3ef7a79f5fa31fbca8df6e0a
Piotr <piotrek.zaranek@gmail.com>
2023-06-13 10:03:03 +0200

    Added NewUserVm class.

---
+++ GiveTheBookBack.Application/ViewModel/NewUserVm.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using GiveTheBookBack.Application.Mapper;
+using GiveTheBookBack.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiveTheBookBack.Application.ViewModel
+{
+    public class NewUserVm : IMapFrom<User>
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<NewUserVm, User>();
+        }
+    }
+}
