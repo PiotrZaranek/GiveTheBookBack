aa631d30f2524c1b541c8d5dfe3a4153b2585eac
Piotr <piotrek.zaranek@gmail.com>
2023-06-13 11:15:26 +0200

    Added UserService without implement methods.

---
+++ GiveTheBookBack.Application/Service/UserService.cs
@@ -0,0 +1,46 @@
+﻿using GiveTheBookBack.Application.Interface;
+using GiveTheBookBack.Application.ViewModel;
+using GiveTheBookBack.Domain.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiveTheBookBack.Application.Service
+{
+    public class UserService : IUserService
+    {
+        readonly private IUserRepository _repository;
+
+        public UserService(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void AddAddress(NewAddressVm addressVm)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Authenticate(LoginUserVm loginModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditAddress(AddressForEditVm addressVm)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AddressForEditVm GetAddressForEdit(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Registration(NewUserVm userVm)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
