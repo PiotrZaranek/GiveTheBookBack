213bf04f0380dd41d641d66f521b5ffdbcb7304a
Piotr <piotrek.zaranek@gmail.com>
2023-06-14 14:39:18 +0200

    Changed return type from Registration method
    From void to RegistrationResultEnum.RegistrationResult.

--- GiveTheBookBack.Application/Interface/IUserService.cs
+++ GiveTheBookBack.Application/Interface/IUserService.cs
@@ -1,5 +1,6 @@
 ﻿using GiveTheBookBack.Application.ViewModel;
 using GiveTheBookBack.Domain.Model;
+using GiveTheBookBack.Domain.Enums;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +12,7 @@ namespace GiveTheBookBack.Application.Interface
     public interface IUserService
     {
         bool Authenticate(LoginModel loginModel);
-        void Registration(NewUserVm userVm);
+        RegistrationResultEnum.RegistrationResult Registration(NewUserVm userVm);
         void AddAddress(NewAddressVm addressVm);
         AddressForEditVm GetAddressForEdit(int id);
         void EditAddress(AddressForEditVm addressVm);
