a786827b5e99a7805348a43612324410354ad2fe
Piotr <piotrek.zaranek@gmail.com>
2023-06-21 16:51:24 +0200

    Added Setup file for controllers tests.

---
+++ GiveTheBookBack.Test/Controllers/Setup.cs
@@ -0,0 +1,21 @@
+﻿using GiveTheBookBack.API.Controllers;
+using GiveTheBookBack.Application.Interface;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiveTheBookBack.Test.Controllers
+{
+    public static class Setup
+    {
+        public static UserController CreateUserController(Mock<IUserService> ser)
+        {
+            var con = new Mock<IConfiguration>();
+            return new UserController(ser.Object, con.Object);
+        }
+    }
+}
